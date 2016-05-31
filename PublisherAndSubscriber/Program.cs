a77e05cdd5e4cde3abf941da0dc30811ec1b0895
fifarafka <wojtasikmonikia@gmail.com>
2016-06-01 00:31:19 +0200

    init

---
+++ PublisherAndSubscriber/Program.cs
@@ -0,0 +1,37 @@
+﻿using EasyNetQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadania10;
+
+namespace PublisherAndSubscriber
+{
+    class Program
+    {
+        public static string NickName;
+
+        static void Main(string[] args)
+        {
+            String instanceId = System.Guid.NewGuid().ToString();
+
+            Console.WriteLine("Give your nickname:");
+            NickName = Console.ReadLine();
+
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.Subscribe<Message>(instanceId, msg => Console.WriteLine($"<{msg.Name}>: {msg.Text}"));
+
+                while (true)
+                {
+                    bus.Publish(new Message
+                    {
+                        Name = NickName,
+                        Text = Console.ReadLine(),
+                    });
+                }
+            }
+        }
+    }
+}
